95bfa9ceeadcbff756843bf1a73d2c4820d6cf57
Nick-Liou <80918856+Nick-Liou@users.noreply.github.com>
2023-12-20 16:08:43 +0200

    Create project and add a simple test

---
+++ cypress/e2e/veg_market_test/dumy_test.cy.js
@@ -0,0 +1,23 @@
+
+
+// cypress/integration/example_spec.js
+
+describe('My Test Suite', function () {
+  it('Visit the website and perform some tests', function () {
+    // Visit the website
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise#/');
+
+    // Perform tests
+    // Example: Verify the title of the page
+    cy.title().should('eq', 'GreenKart - veg and fruits kart');
+
+    // Example: Perform actions like clicking on elements
+    cy.get('.search-keyword').type('ca');
+
+    // Example: Assertion after an action
+    // Use a more specific selector to target the products within the products wrapper
+    cy.get('.products .product').should('have.length', 4);
+
+    // Add more tests based on your requirements
+  });
+});
