7f627839158c50718f1479de766a69ac7c396d7b
Nick-Liou <80918856+Nick-Liou@users.noreply.github.com>
2023-12-20 17:21:42 +0200

    Get the names and prices of all products and store them to the json. Also add a test that adds every product to the cart one by one checks the price is as expected and then reloads the page to add the next product

--- cypress/e2e/veg_market_test/dumy_test.cy.js
+++ cypress/e2e/veg_market_test/dumy_test.cy.js
@@ -1,7 +1,4 @@
 
-
-// cypress/integration/example_spec.js
-
 describe('My Test Suite', function () {
   it('Visit the website and perform some tests', function () {
     // Visit the website
@@ -22,7 +19,6 @@ describe('My Test Suite', function () {
   });
 });
 
-// cypress/integration/cart_spec.js
 
 describe('Add Products to Cart', function () {
   beforeEach(function () {
@@ -40,40 +36,86 @@ describe('Add Products to Cart', function () {
   });
 });
 
-// cypress/integration/cart_spec.js
 
-describe('Add Products to Cart 2', function () {
+describe('Add the First Product to Cart', function () {
   beforeEach(function () {
     // Visit the website before each test
     cy.visit('https://rahulshettyacademy.com/seleniumPractise#/');
   });
 
-  
-  it('should add a single product to the cart', function () {
-    // Load the product fixture
-    cy.fixture('product').then((product) => {
-      // Add a product to the cart
+
+  it('should add the first product to the cart', function () {
+    // Load the products fixture
+    cy.fixture('products').then((products) => {
+      // Take only the first product from the JSON array
+      const product = products[0];
+
+      // Type the product name in the search field
       cy.get('.search-keyword').type(product.name);
-  
-      // Wait for the search to complete
+
+      // Wait for the search to complete (you can improve this by waiting for a specific element)
       cy.wait(1000); // Adjust the wait time as needed
-  
+
+      // Click the "ADD TO CART" button for the first product
       cy.get('.products .product').first().contains('ADD TO CART').click();
-  
+
       // Wait for the cart to be updated and then make assertions
       cy.get('.cart-info table tbody tr td strong').should('contain', '1'); // Check if the cart has 1 item
-  
+
       // Add more assertions based on your specific implementation for total amount calculation
       // For example, use the product price from the fixture
       cy.get(':nth-child(2) > :nth-child(3) > strong').should('contain', product.price); // Check if the cart amount is correct
-  
+
       // Check if the "PROCEED TO CHECKOUT" button is enabled
       cy.get('.cart-preview button').should('not.be.disabled');
     });
   });
+
+
+});
+
+
+
+
+describe('Add Products to Cart', function () {
+  beforeEach(function () {
+    // Visit the website before each test
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise#/');
+  });
   
 
-  // You can add more tests for different scenarios, such as adding multiple products, removing products, etc.
+  it('should add every product to the cart', function () {
+    // Load the products fixture
+    cy.fixture('products').then((products) => {
+      // Iterate through each product
+      products.forEach((product) => {
+        // Reload the page before adding each product
+        cy.reload();
+
+        // Type the product name in the search field
+        cy.get('.search-keyword').type(product.name);
+
+        // Wait for the search to complete (you can improve this by waiting for a specific element)
+        cy.wait(1000); // Adjust the wait time as needed
+
+        // Click the "ADD TO CART" button for the first product
+        cy.get('.products .product').first().contains('ADD TO CART').click();
+
+        // Wait for the cart to be updated and then make assertions
+        cy.get('.cart-info table tbody tr td strong').should('contain', '1'); // Check if the cart has 1 item
+
+        // Add more assertions based on your specific implementation for total amount calculation
+        // For example, use the product price from the fixture
+        cy.get(':nth-child(2) > :nth-child(3) > strong').should('contain', product.price); // Check if the cart amount is correct
+
+        // Check if the "PROCEED TO CHECKOUT" button is enabled
+        cy.get('.cart-preview button').should('not.be.disabled');
+
+        // Clear the search field for the next iteration
+        cy.get('.search-keyword').clear();
+      });
+    });
+  });
 });
 
 
