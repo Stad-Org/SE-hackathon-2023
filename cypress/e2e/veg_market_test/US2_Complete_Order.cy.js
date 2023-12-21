27a67ed53c0f35b4e75c0010ae23a462b24f0ab7
deepblue597 <iasonas2001@hotmail.com>
2023-12-21 13:45:32 +0200

    refactored test for better readability

--- cypress/e2e/veg_market_test/US2_Complete_Order.cy.js
+++ cypress/e2e/veg_market_test/US2_Complete_Order.cy.js
@@ -1,85 +1,99 @@
-
-describe('As a user I want to create and complete the order for the products I have in the cart',  () => {
-    beforeEach( () => {
+describe('As a user I want to create and complete the order for the products I have in the cart', () => {
+    beforeEach(() => {
       // Visit the website before each test
       cy.visit('https://rahulshettyacademy.com/seleniumPractise#/');
     });
-
-    
-    it('should place an order',  () => {
-        // Load the products fixture
-        cy.fixture('products').then((products) => {
-            // Take only the first product from the JSON array
-            const product = products[0];
-
-            // Type the product name in the search field
-            cy.get('.search-keyword').type(product.name);
-
-            // Every search here should yield only one result
-            cy.get('.products .product').its('length').should('eq', 1); 
-
-            // Check that product has correct name and price
-            cy.get('.products .product').first().find('.product-name').should('contain', product.name);
-            cy.get('.products .product').first().find('.product-price').should('contain', product.price);
-
-            // Find the product with product.name and add it to cart 
-            cy.contains('.product .product-name', product.name)
-                .parents('.product')
-                .find('.product-action button')
-                .contains('ADD TO CART')
-                .click();
-
-
-            // Click on the cart icon to go to the cart page
-            cy.get('.cart-icon').click();
-
-            // Validate that products are added to the cart
-            cy.get('ul li').first().find('.product-name').should('contain', product.name);
-            cy.get('ul li').first().find('.product-price').should('contain', product.price);
-            
-            // Get the price and quantity values
-            cy.get('.cart-preview .product-name')
-            .siblings('.product-price')
+  
+    function addProductToCart(product) { 
+      // Type the product name in the search field
+      cy.get('.search-keyword').type(product.name);
+  
+      // Every search here should yield only one result
+      cy.get('.products .product').its('length').should('eq', 1);
+  
+      // Check that product has correct name and price
+      cy.get('.products .product').first().find('.product-name').should('contain', product.name);
+      cy.get('.products .product').first().find('.product-price').should('contain', product.price);
+  
+      // Find the product with product.name and add it to cart
+      cy.contains('.product .product-name', product.name)
+        .parents('.product')
+        .find('.product-action button')
+        .contains('ADD TO CART')
+        .click();
+    };
+  
+    function validateProductInCart(productName, productPrice) {
+      // Click on the cart icon to go to the cart page
+      cy.get('.cart-icon').click();
+  
+      // Validate that products are added to the cart
+      cy.get('ul li').first().find('.product-name').should('contain', productName);
+      cy.get('ul li').first().find('.product-price').should('contain', productPrice);
+    };
+  
+    function validateTotalAmount(expectedTotalAmount) {
+      cy.get('.cart-preview .product-name')
+        .siblings('.product-price')
+        .invoke('text')
+        .then((priceText) => {
+          const price = parseFloat(priceText.replace('₹', ''));
+  
+          cy.get('.cart-preview .product-total .quantity')
             .invoke('text')
-            .then((priceText) => {
-            const price = parseFloat(priceText.replace('₹', '')); // Assuming the price is in the format '₹x.xx'
-
-            cy.get('.cart-preview .product-total .quantity')            
-                .invoke('text')
-                .then((quantityText) => {
-                    const quantity = parseInt(quantityText, 10);
-
-                    // Calculate the expected total amount
-                    const expectedTotalAmount = price * quantity;
-
-                    // Retrieve the actual total amount from the cart
-                    cy.get('.cart-preview .product-total .amount')
-                        .invoke('text')
-                        .then((actualTotalAmountText) => {
-                            const actualTotalAmount = parseFloat(actualTotalAmountText.replace('₹', '')); // Assuming the total amount is in the format '₹x.xx'
-
-                            // Compare the expected and actual total amounts
-                            expect(actualTotalAmount).to.equal(expectedTotalAmount);
-
-                            // Optionally, you can log or perform additional actions based on the comparison result
-                            cy.log(`Expected Total Amount: ₹${expectedTotalAmount}`);
-                            cy.log(`Actual Total Amount: ₹${actualTotalAmount}`);
-
-                            cy.contains('PROCEED TO CHECKOUT').click();
-
-                                
-                            cy.contains('Place Order').click();
-                            cy.get('select').select('Greece');
-                            cy.get('[type="checkbox"]').check() 
-                            cy.contains('Proceed').click();
-                            
-                            cy.wait(6000); // Adding a wait to allow time for the suggestion to appear
-
-                            cy.url().should('eq', 'https://rahulshettyacademy.com/seleniumPractise/#/')
-
-                    });
-                });
+            .then((quantityText) => {
+              const quantity = parseInt(quantityText, 10);
+  
+              // Calculate the expected total amount
+              const actualTotalAmount = price * quantity;
+  
+              // Compare the expected and actual total amounts
+              expect(actualTotalAmount).to.equal(expectedTotalAmount);
             });
         });
-    })
-}) 
+    };
+  
+    function validateCheckoutPage(productName, quantity, productPrice, actualTotalAmount) {
+      cy.contains('PROCEED TO CHECKOUT').click();
+  
+      cy.get('.products table .product-name').should('contain', productName);
+      cy.get('.products table .quantity').should('contain', quantity);
+      cy.get('.products table .amount').first().should('contain', productPrice);
+      cy.get('.products table .amount').last().should('contain', actualTotalAmount);
+    };
+  
+    const completeOrder = () => {
+      cy.contains('Place Order').click();
+      cy.get('select').select('Greece');
+      cy.get('[type="checkbox"]').check();
+      cy.contains('Proceed').click();
+  
+      cy.wait(6000); // Adding a wait to allow time for the suggestion to appear
+  
+      cy.url().should('eq', 'https://rahulshettyacademy.com/seleniumPractise/#/');
+    };
+  
+    it('should place an order', () => {
+      // Load the products fixture
+      cy.fixture('products').then((products) => {
+        // Take only the first product from the JSON array
+        const product = products[0];
+  
+        // Step 1: Add product to cart
+        addProductToCart(product);
+  
+        // Step 2: Validate product in the cart
+        validateProductInCart(product.name, product.price);
+  
+        // Step 3: Validate total amount
+        validateTotalAmount(product.price * 1); // Assuming quantity is 1
+  
+        // Step 4: Validate checkout page
+        validateCheckoutPage(product.name, 1, product.price, product.price * 1);
+  
+        // Step 5: Complete the order
+        completeOrder();
+      });
+    });
+  });
+  
