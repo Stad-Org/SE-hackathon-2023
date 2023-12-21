74d89b12c8082516276be05d02014fb7c656dcaa
Panos Syskakis <75637426+Pansysk75@users.noreply.github.com>
2023-12-21 21:57:26 +0200

    Fix broken US3 test

--- cypress/e2e/veg_market_test/US3_Search_for_Products.cy.js
+++ cypress/e2e/veg_market_test/US3_Search_for_Products.cy.js
@@ -50,8 +50,14 @@ describe('Search for Products', function () {
         // Type the substring in the search field
         cy.get('.search-keyword').type(searchSubstring);
 
+        // Get expected results
+        const expectedResults = products.filter((product) => {
+        return product.name.includes(searchSubstring);
+        });
+
+
         // Verify that search results contain all products with the search substring
-        cy.get('.products .product').should('have.length.greaterThan', 0);
+        cy.get('.products .product').should('have.length', expectedResults.length);
         cy.get('.products .product .product-name').each(($el) => {
         cy.wrap($el.text()).should('contain', searchSubstring);
         });
