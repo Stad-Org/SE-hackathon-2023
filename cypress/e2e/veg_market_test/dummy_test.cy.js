5df15bbbd44a2aa8cdecdcf48cdd3c2fceb9d65b
Nick-Liou <80918856+Nick-Liou@users.noreply.github.com>
2023-12-21 19:49:50 +0200

    Move a test to US1 from dummy and make a new one

--- cypress/e2e/veg_market_test/dummy_test.cy.js
+++ cypress/e2e/veg_market_test/dummy_test.cy.js
@@ -81,51 +81,6 @@ describe('Add the First Product to Cart', function () {
 
 
 
-describe('Add Products to Cart', function () {
-  beforeEach(function () {
-    // Visit the website before each test
-    cy.visit('https://rahulshettyacademy.com/seleniumPractise#/');
-  });
-  
-
-  it('should add every product to the cart', function () {
-    // Load the products fixture
-    cy.fixture('products').then((products) => {
-      // Iterate through each product
-      products.forEach((product) => {
-        // Reload the page before adding each product
-        cy.reload();
-
-        // Type the product name in the search field
-        cy.get('.search-keyword').type(product.name);
-
-        // Every search here should yield only one result
-        cy.get('.products .product').its('length').should('eq', 1); 
-
-        // Check that product has correct name and price
-        cy.get('.products .product').first().find('.product-name').should('contain', product.name);
-        cy.get('.products .product').first().find('.product-price').should('contain', product.price);
-
-        // Click the "ADD TO CART" button for the first product
-        cy.get('.products .product').first().contains('ADD TO CART').click();
-
-        // Wait for the cart to be updated and then make assertions
-        cy.get('.cart-info table tbody tr td strong').should('contain', '1'); // Check if the cart has 1 item
-
-        // Add more assertions based on your specific implementation for total amount calculation
-        // For example, use the product price from the fixture
-        cy.get(':nth-child(2) > :nth-child(3) > strong').should('contain', product.price); // Check if the cart amount is correct
-
-        // Check if the "PROCEED TO CHECKOUT" button is enabled
-        cy.get('.cart-preview button').should('not.be.disabled');
-
-        // Clear the search field for the next iteration
-        cy.get('.search-keyword').clear();
-      });
-    });
-  });
-});
-
 
 
 // describe('Add Products to Cart', function () {
