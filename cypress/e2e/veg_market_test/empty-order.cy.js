8e26a63f9301063b003d0060e56e88b9159127c7
deepblue597 <iasonas2001@hotmail.com>
2023-12-22 13:29:54 +0200

    empty order proceed to checkout button should be disabled

--- cypress/e2e/veg_market_test/empty-order.cy.js
+++ cypress/e2e/veg_market_test/empty-order.cy.js
@@ -1,29 +1,30 @@
-describe('Empty Order Test', () => {
-    beforeEach(() => {
-      // Visit the website before each test
-      cy.visit('https://rahulshettyacademy.com/seleniumPractise#/');
-    });
-  
-    it('should create an empty order', () => {
-      // Step 1: Navigate to the checkout page
-      cy.get('.cart-icon').click();
-      cy.contains('PROCEED TO CHECKOUT').click();
-  
-      // Step 2: Verify that the checkout page is displayed
-      cy.url().should('include', '/cart');
-  
-      // Step 3: Click on 'Place Order' on the checkout page
-      cy.contains('Place Order').click();
+describe("Empty Order Test", () => {
+	beforeEach(() => {
+		// Visit the website before each test
+		cy.visit("https://rahulshettyacademy.com/seleniumPractise#/");
+	});
 
+	it("should create an empty order", () => {
+		// Step 1: Navigate to the checkout page
+		cy.get(".cart-icon").click();
 
-  
-      // Step 4: Check the checkbox and click on 'Proceed'
-      cy.get('[type="checkbox"]').check();
-      cy.contains('Proceed').click();
-  
-      cy.wait(6000);
-  
-      // Step 6: Verify redirection to the home page
-      cy.url().should('eq', 'https://rahulshettyacademy.com/seleniumPractise/#/');
-    });
-  });
+		cy.contains("PROCEED TO CHECKOUT").should("be.disabled");
+		cy.contains("PROCEED TO CHECKOUT").click();
+
+		// Step 2: Verify that the checkout page is displayed
+		cy.url().should("include", "/cart");
+
+		// Step 3: Click on 'Place Order' on the checkout page
+
+		cy.contains("Place Order").click();
+
+		// Step 4: Check the checkbox and click on 'Proceed'
+		cy.get('[type="checkbox"]').check();
+		cy.contains("Proceed").click();
+
+		cy.wait(6000);
+
+		// Step 6: Verify redirection to the home page
+		cy.url().should("eq", "https://rahulshettyacademy.com/seleniumPractise/#/");
+	});
+});
